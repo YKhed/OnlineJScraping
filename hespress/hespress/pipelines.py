f5f7388500af0ab908a041cff7c71f23c5fb05bf
unknown <y.khedid@gmail.com>
2021-07-02 11:21:19 +0200

    v2

--- hespress/hespress/pipelines.py
+++ hespress/hespress/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import pymongo
 
 
-class HespressPipeline(object):
+class MongodbPipeline(object):
+    collection_name = "comments"
+    
+    def open_spider(self, spider) : 
+        self.client = pymongo.MongoClient("mongodb+srv://scrapyYK:Jabouralol1.@cluster0.ac6mx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        self.db = self.client("HESPRESS")
+
+    def close_spider(self, spider) :
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection.name].insert(item)
         return item
